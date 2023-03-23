ed2e06a876d46ad307c441a073f7fa83f46d7b11
Matheus <math.a.monteiro@gmail.com>
2023-03-23 10:44:07 -0300

    first attempt at parallelization

--- bubble_parallel.c
+++ bubble_parallel.c
@@ -6,8 +6,34 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <pthread.h>
 
 #define IMPRIME
+#define NUM_THREADS 4
+
+// definição de struct para passagem de argumentos para a função da thread
+typedef struct
+{
+  int *array;
+  int n_arrays;
+  int tam;
+  int thread_num;
+} t_args;
+
+// definição da função da thread
+void *t_function(void *arg)
+{
+  int i;
+  t_args *targ;
+  targ = (t_args *)arg;
+
+  for (i = targ->thread_num * targ->n_arrays / NUM_THREADS; i < (targ->thread_num + 1) * targ->n_arrays / NUM_THREADS; i++)
+  {
+    bubble(targ->array[i], targ->tam);
+  }
+
+  pthread_exit(NULL);
+}
 
 void imprime(int **array, int num_arrays, int size)
 {
@@ -42,6 +68,9 @@ int bubble(int *array, unsigned int size)
 
 int main(int argc, char **argv)
 {
+  // inicialização de array de threads e argumentos
+  pthread_t t[NUM_THREADS];
+  t_args *targs[NUM_THREADS];
   int **elementos, n, tam, i, j;
   struct timeval t1, t2;
   double t_total;
@@ -73,8 +102,27 @@ int main(int argc, char **argv)
 #endif
 
   gettimeofday(&t1, NULL);
-  for (i = 0; i < n; i++)
-    bubble(elementos[i], tam);
+
+  for (i = 0; i < NUM_THREADS; i++)
+  {
+    // aloca memória e configura argumentos
+    targs[i] = (t_args *)malloc(sizeof(t_args));
+    targs[i]->array = elementos;
+    targs[i]->tam = tam;
+    targs[i]->thread_num = i;
+
+    printf("Thread %d sendo criada\n", i);
+    pthread_create(&t[i], NULL, t_function, (void *)targs[i]);
+  }
+  printf("Aguardando termino das threads\n");
+
+  for (i = 0; i < NUM_THREADS; i++)
+    pthread_join(t[i], NULL);
+
+  // libera memória alocada para argumentos
+  for (i = 0; i < NUM_THREADS; i++)
+    free(targs[i]);
+
   gettimeofday(&t2, NULL);
 
   t_total = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1000000.0);
